381acb068ebebc357edf80e28e5208481423b4c1
Ethan Kennelly <ethan.kennelly@stevechristians.octanner.com>
2013-06-24 13:13:42 -0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kata::Application.config.secret_token = 'fac8c635b6c1938995f440d6b4aabf0d382dcf19d55e85a4913ec8a856f9e2a92b03c2f66887ef740efc128f681054fc7b361e96f7e4fc89bce6cada4b31b464'
